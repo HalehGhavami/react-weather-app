7eebff71bdedf9604b55aec95dc36930c73508a3
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2023-01-05 23:37:42 +0330

    refactored

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
+import Weather from './components/Weather';
 // import Forcast from './components/Forcast';
 // import Navigation from './components/Navigation';
-import ResultWeather from './components/ResultWeather';
-// import SearchEngine from './components/SearchEngine';
 // import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      {/* <Navigation /> */}
-      {/* <SearchEngine /> */}
-      <ResultWeather />
-      {/* <Forcast /> */}
+      <div className="container weather-container">
+        <h1>Weather App</h1>
+        {/* <Navigation /> */}
+        <Weather />
+        {/* <Forcast /> */}
+      </div>
+      <footer>
+        <small>
+          <a
+            href="https://github.com/HalehGhavami/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Haleh Ghavami
+        </small>
+      </footer>
     </div>
   );
 }
